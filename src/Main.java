4cf793308126bd759aebfa3a679826b1bb430614
Sergey Boyko <sergejuswfb@gmail.com>
2023-04-10 15:41:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 45000;
+        int bonus = 20;
+        int result = amount / bonus;
+
+        System.out.println("Ваши бонусные мили: " + result);
+          }
+}
